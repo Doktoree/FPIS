4282ec3f09a37196fc74824fe073b26006451f5a
doktoree <tjuring901@gmail.com>
2024-06-29 13:41:43 +0200

    Implementirane su klase ProductController i ProductService

---
+++ backend/src/main/java/com/projekat/fpis/controller/ProductController.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.projekat.fpis.controller;
+
+import com.projekat.fpis.dto.ProductDto;
+import com.projekat.fpis.repository.ProductRepository;
+import com.projekat.fpis.service.ProductService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Lav
+ */
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<?> getAllProducts() {
+
+        List<ProductDto> products = productService.getAllProducts();
+
+        if (products == null) 
+            return ResponseEntity.badRequest().body("There are no products");
+        
+
+        return ResponseEntity.ok(products);
+
+    }
+    
+    
+    @GetMapping("{id}")
+    public ResponseEntity<?> getProductById(@PathVariable Long id){
+        
+        ProductDto product = productService.getProductById(id);
+        
+        if(product == null)
+            return ResponseEntity.badRequest().body("There is no product with given ID!");
+        
+        return ResponseEntity.ok(product);
+    }
+    
+    @GetMapping("/search")
+    public ResponseEntity<?> getProductByExample(@RequestBody ProductDto productDto){
+        
+        List<ProductDto> productDtos = productService.getProductByExample(productDto);
+        
+        if(productDtos == null)
+            return ResponseEntity.badRequest().body("There are no products for given example!");
+        
+        return ResponseEntity.ok(productDtos);
+        
+    }
+    
+    @PostMapping
+    public ResponseEntity<?> createProduct(@RequestBody ProductDto productDto){
+        
+        ProductDto createdProductDto = productService.createProduct(productDto);
+        
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdProductDto);
+        
+    }
+    
+    
+    @PatchMapping("{id}")
+    public ResponseEntity<?> saveProduct(@PathVariable Long id, @RequestBody ProductDto productDto){
+        
+        ProductDto savedProductDto = productService.saveProduct(productDto, id);
+        
+        if(savedProductDto == null)
+            return ResponseEntity.badRequest().body("There is no product with given ID!");
+        
+        return ResponseEntity.ok(savedProductDto);
+        
+    }
+
+    
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> deleteProduct(@PathVariable Long id){
+        
+        if(!productService.deleteProduct(id))
+            return ResponseEntity.badRequest().body("There is no product with given ID!");
+        
+        return ResponseEntity.ok("Product is succesfully deleted!");
+        
+        
+    }
+    
+}
---
+++ backend/src/main/java/com/projekat/fpis/service/ProductService.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.projekat.fpis.service;
+
+import com.projekat.fpis.domain.Product;
+import com.projekat.fpis.dto.ProductDto;
+import com.projekat.fpis.mapper.ProductMapper;
+import com.projekat.fpis.repository.ProductRepository;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Lav
+ */
+@Service
+public class ProductService {
+    
+    
+    @Autowired
+    private ProductRepository productRepository;
+    
+    
+    public List<ProductDto> getAllProducts(){
+        
+        List<Product> products = productRepository.findAll();
+        List<ProductDto> productDtos = products.stream().map(ProductMapper::mapToProductDto).collect(Collectors.toList());
+
+        return productDtos;
+    }
+    
+    
+    public ProductDto getProductById(Long id){
+        
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        
+        if(!optionalProduct.isPresent())
+            return null;
+        
+        Product product = optionalProduct.get();
+        
+        return ProductMapper.mapToProductDto(product);
+    }
+    
+    public List<ProductDto> getProductByExample(ProductDto productDto){
+        
+        Product product = ProductMapper.mapToProduct(productDto);
+        
+        List<Product> products = productRepository.findAll(Example.of(product));
+        
+        List<ProductDto> productDtos =  products.stream().map(ProductMapper::mapToProductDto).collect(Collectors.toList());
+        
+        return productDtos;
+    }
+    
+    public ProductDto createProduct(ProductDto productDto){
+        
+        Product product = ProductMapper.mapToProduct(productDto);
+        
+        Product createdProduct = productRepository.save(product);
+        
+        return ProductMapper.mapToProductDto(createdProduct);
+        
+    }
+    
+    
+    public ProductDto saveProduct(ProductDto productDto, Long id){
+        
+        Optional<Product> product = productRepository.findById(id);
+        
+        if(product.isPresent()){
+            
+            Product updatedProduct = product.get();
+            updatedProduct.setName(productDto.getName());
+            updatedProduct.setDescription(productDto.getDescription());
+            updatedProduct.setPrice(productDto.getPrice());
+            
+            Product savedProduct = productRepository.save(updatedProduct);
+            return ProductMapper.mapToProductDto(savedProduct);
+            
+            
+        }
+        
+        return null;
+    }
+    
+    public boolean deleteProduct(Long id){
+        
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        
+        if(!optionalProduct.isPresent())
+            return false;
+        
+        productRepository.deleteById(id);
+        
+        return true;
+        
+    }
+}
