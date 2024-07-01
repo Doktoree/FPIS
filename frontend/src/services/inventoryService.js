acf6c83382e55f37e325d4e3da1995c77b46f38e
doktoree <tjuring901@gmail.com>
2024-07-01 17:25:10 +0200

    Dodat invetoryService

---
+++ frontend/src/services/inventoryService.js
@@ -0,0 +1,56 @@
+const apiUrl = "http://localhost:8080/api/inventory"
+
+export async function getAllInventories(){
+
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    return data;
+
+}
+
+
+export async function getInventoryById(id){
+
+    const response = await fetch(apiUrl + "/" + id);
+    const data = await response.json();
+
+    return data;
+}
+
+export async function createInventory(inventory){
+
+    const response = await fetch(apiUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(inventory),
+      });
+
+    const data = response.json();
+
+    return data;
+
+}
+
+
+export async function saveInventory(id, inventory){
+
+    const response = await fetch(apiUrl + "/" + id, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(inventory),
+      });
+    
+      if (!response.ok) {
+        throw new Error("There is no product with given ID!");
+      }
+      const data = await response.json();
+    
+      return data;
+
+}
+
